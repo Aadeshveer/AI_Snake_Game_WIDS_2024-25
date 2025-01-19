633b1189b7c7e62ef08bde79a031c620a38fdce0
Aadeshveer Singh <aadeshveer07@gmail.com>
2025-01-19 14:36:10 +0530

    Update Human_game.py

--- Week 3-4/Human_game.py
+++ Week 3-4/Human_game.py
@@ -16,11 +16,7 @@ class Direction(Enum):
 Point = namedtuple('Point', 'x, y')
 
 # rgb colors
-WHITE = (255, 255, 255)
-RED = (200,0,0)
-BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
-BLACK = (0,0,0)
+TEXT_COLOR = (255, 255, 255)
 BACK_GROUND = (221, 227, 52)
 
 
@@ -139,7 +135,7 @@ class SnakeGame:
         
         self.display.blit(self.apple,self.food)
        
-        text = font.render("Score: " + str(self.score), True, WHITE)
+        text = font.render("Score: " + str(self.score), True, TEXT_COLOR)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
         
